2b3c88503a4b269e97bfea349dbbc8cf0c5dab1e
yafeunteun <yannfeunteun@gmail.com>
2017-04-20 11:50:17 +0200

    Bootstrap assgn 5

---
+++ Assignment-5-Draw-Graphics/draw-graphics/src/main/java/edu/mit/yfe/App.java
@@ -0,0 +1,13 @@
+package edu.mit.yfe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
