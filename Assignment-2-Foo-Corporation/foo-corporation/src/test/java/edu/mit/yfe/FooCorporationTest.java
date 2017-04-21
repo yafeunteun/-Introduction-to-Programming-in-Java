07077ba142c0e9e2bda7b8a0c52b40bebc8a55f9
yafeunteun <yannfeunteun@gmail.com>
2017-04-21 15:08:00 +0200

    Added Tests for assgn 2

---
+++ Assignment-2-Foo-Corporation/foo-corporation/src/test/java/edu/mit/yfe/FooCorporationTest.java
@@ -0,0 +1,51 @@
+package edu.mit.yfe;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for FooCorporation
+ */
+public class FooCorporationTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public FooCorporationTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( FooCorporation.class );
+    }
+
+
+    public void testFooCorporationLessThan40HoursWorkWeek()
+    {
+	double salary = FooCorporation.computeTotalPay(7.50, 35);
+	assertTrue( salary < 263 && salary > 262);
+    }
+
+    public void testFooCorporationMoreThan40HoursWorkWeekButLessThan60HoursWorkWeek()
+    {
+	double salary = FooCorporation.computeTotalPay(8.20, 47);
+	assertTrue( salary < 415 && salary > 414);
+    }
+
+    public void testFooCorporationMoreThan60HoursWorkWeek()
+    {
+	try {
+	    double salary = FooCorporation.computeTotalPay(10.00, 73);
+	    fail("Exception not thrown!");
+	} catch(YouWorkTooMuchException crazyGuy){}
+    }
+}
