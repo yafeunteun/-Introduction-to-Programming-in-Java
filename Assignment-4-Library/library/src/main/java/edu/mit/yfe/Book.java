5e64320542434794d12b276b6044789887d63ff6
yafeunteun <yannfeunteun@gmail.com>
2017-04-24 13:31:30 +0200

    Added Book API without method implementation

---
+++ Assignment-4-Library/library/src/main/java/edu/mit/yfe/Book.java
@@ -0,0 +1,28 @@
+package edu.mit.yfe;
+
+public class Book {
+
+    String title;
+    boolean borrowed;
+    
+    // Creates a new Book
+    public Book(String bookTitle){
+	// Implement this method
+    }
+    // Marks the book as rented
+    public void borrowed() {
+	// Implement this method
+    }
+    // Marks the book as not rented
+    public void returned() {
+	// Implement this method
+    }
+    // Returns true if the book is rented, false otherwise
+    public boolean isBorrowed() {
+	return true;
+    }
+    // Returns the title of the book
+    public String getTitle() {
+	return "";
+    }
+}
